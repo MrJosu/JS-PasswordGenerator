0c025e5e75c8c4220c6b9e1396da57f970314473
Josu <meccu19@gmail.com>
2023-01-15 21:25:01 +0000

    Beta version of Passgenerator

--- script.js
+++ script.js
@@ -1,93 +1,11 @@
 // Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 // Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
-
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 // Function to prompt user for password options
 function getPasswordOptions(){
 let pwdLength = parseInt (
@@ -132,47 +50,45 @@ let passwordOptions = {
   includeLowerCasedCharacters: includeLowerCasedCharacters,
   includeUpperCasedCharacters: includeUpperCasedCharacters
 }
-
 return passwordOptions;
 }
-
 // Function for getting a random element from an array
 function getRandom(arr) {
-let randomSet = Math.floor(Math.random() * arr.length)
+let randomSet = Math.floor(Math.random() * arr.length);
 let randomElement = arr[randomSet];
-return randomElement
+return randomElement;
 }
-
 // Function to generate password with user input
 function generatePassword() {
 let options = getPasswordOptions();
-console.log(options)
-let pwdGenerationResult = []
-
-let possibleCharacters = []
-
-let guaranteedCharacters = []
-
+let pwdGenerationResult = [];
+let possibleCharacters = [];
+let guaranteedCharacters = [];
 if(options.includeSpecialCharacters) {
   possibleCharacters = possibleCharacters.concat(specialCharacters)
   guaranteedCharacters.push(getRandom(specialCharacters))
-} 
+}
 if(options.includeNumericCharacters) {
+  possibleCharacters = possibleCharacters.concat(numericCharacters)
+  guaranteedCharacters.push(getRandom(numericCharacters))
+}
+if(options.includeLowerCasedCharacters) {
   possibleCharacters = possibleCharacters.concat(lowerCasedCharacters)
   guaranteedCharacters.push(getRandom(lowerCasedCharacters))
-} 
-console.log(guaranteedCharacters)
-console.log(possibleCharacters)
-
-
-
-
-
-
-
-
 }
-
+if(options.includeUpperCasedCharacters) {
+  possibleCharacters = possibleCharacters.concat(upperCasedCharacters)
+  guaranteedCharacters.push(getRandom(upperCasedCharacters))
+}
+console.log(possibleCharacters)
+console.log(guaranteedCharacters)
+for(let i = 0; i < options.pwdLength; i++){
+  var generated = getRandom(possibleCharacters);
+  pwdGenerationResult.push(generated);
+}
+return pwdGenerationResult.join("")
+}
+// console.log(pwdGenerationResult)
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
@@ -180,7 +96,6 @@ var generateBtn = document.querySelector('#generate');
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
   passwordText.value = password;
 }
 
